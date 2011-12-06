0d92259ccff88ab0299c649d41be9aa567c4c14a
JBirdVegas <jbirdvegas@gmail.com>
2011-12-06 00:33:43 -0500

    ICS edition starts here
    I hate to loose all the previous hard work but now we are all starting
    from the same base
    
    sorry frameworks most in 4.01 you don't exist!

--- Android.mk
+++ Android.mk
@@ -2,11 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := LiquidSettings
-LOCAL_CERTIFICATE := platform
+LOCAL_PACKAGE_NAME := ChurchKey
+
 include $(BUILD_PACKAGE)
 
-# Use the folloing include to make our test apk.
-# include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ src/com/n00bware/churchkey/Global.java
@@ -0,0 +1,13 @@
+
+package com.n00bware.parchment;
+
+public class Global {
+
+    private Global() {
+        //private construct
+    }
+
+    public static int TRACKER;
+
+
+}
