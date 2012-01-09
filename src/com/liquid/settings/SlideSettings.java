bb247a7915738b3a79f001684cfd235511550d7b
JBirdVegas <jbirdvegas@gmail.com>
2012-01-09 13:30:34 -0500

    this is a massive revert of everything that requires frameworks support
    I also reverted all the lists except Nom-Nom, Extras and lockscreen
    ->Lockscreen is the only empty list but we can add lists as needed

--- src/com/liquid/settings/SlideSettings.java
+++ src/com/liquid/settings/SlideSettings.java
@@ -58,15 +58,7 @@ import android.content.Intent;
 
 import com.liquid.settings.lists.LiquidNomNomList;
 import com.liquid.settings.lists.ExtrasList;
-import com.liquid.settings.lists.ApplicationList;
-import com.liquid.settings.lists.DisplayList;
-import com.liquid.settings.lists.InputList;
-import com.liquid.settings.lists.InterfaceList;
 import com.liquid.settings.lists.LockscreenList;
-import com.liquid.settings.lists.PerformanceList;
-import com.liquid.settings.lists.SoundList;
-import com.liquid.settings.lists.SystemList;
-import com.liquid.settings.lists.TabletList;
 import com.liquid.settings.lists.MasterLists;
 
 public class SlideSettings extends Activity {
@@ -83,15 +75,7 @@ public class SlideSettings extends Activity {
     static {
         listHead.put(0, new LiquidNomNomList());
         listHead.put(1, new ExtrasList());
-        listHead.put(2, new ApplicationList());
-        listHead.put(3, new DisplayList());
-        listHead.put(4, new InputList());
-        listHead.put(5, new InterfaceList());
         listHead.put(6, new LockscreenList());
-        listHead.put(7, new PerformanceList());
-        listHead.put(8, new SoundList());
-        listHead.put(9, new SystemList());
-        listHead.put(10, new TabletList());
     }
 
     @Override
@@ -142,8 +126,7 @@ public class SlideSettings extends Activity {
     }
     
     public static class SettingsAdapter extends FragmentPagerAdapter {
-        private final String[] tabs = {"Liquid Nom-Nom", "Extras", "Application", "Display", "Input", "Interface", "Lockscreen", 
-                                  "Performace", "Sound", "System", "Tablet" };
+        private final String[] tabs = {"Nom-Nom", "Extras",  "Lockscreen"};
 
         public SettingsAdapter(FragmentManager fm) {
             super(fm);
@@ -195,7 +178,7 @@ public class SlideSettings extends Activity {
             super.onActivityCreated(savedInstanceState);
             List<Headers> aHeaders = new ArrayList<Headers>();
             ArrayList<MasterLists.List> mHeaders = listHead.get(mNum).getList();
-            
+
             if(!mHeaders.isEmpty()) {
                 for(MasterLists.List mObj : mHeaders) {
                     Headers header = new Headers(mObj.titleResId,
@@ -334,11 +317,11 @@ public class SlideSettings extends Activity {
                     case HEADER_TYPE_CATEGORY:
                         holder.title.setText(mContext.getResources().getString(header.mTitle));
                         break;
-
                     case HEADER_TYPE_SWITCH:
                         if (header.mTitle == R.string.title_expanded_widget) {
                              mWidgetEnabler.setSwitch(holder.switch_);
                         }
+
                         //$FALL-THROUGH$
                     case HEADER_TYPE_NORMAL:
                         //holder.icon.setImageResource(header.iconRes);
